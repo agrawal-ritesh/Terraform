fb8c67639d408846c3cd8ee9dd37c8ab7957ffc2
Ritesh DevOps <riteshagarwal1497@gmail.com>
2023-09-26 23:58:54 +0530

    Terraformprovider Azure

---
+++ terraformprovider.tf
@@ -0,0 +1,11 @@
+terraform {
+    required_providers{
+        azurerm = {
+            source = "Hashicorp/azurerm"
+            version =  "3.75.0"
+        }
+    }
+}
+provider "azurerm"{
+    
+}
