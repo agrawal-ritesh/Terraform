4e177796e9358d24177a725784ab8368f10aa2b1
Ritesh DevOps <riteshagarwal1497@gmail.com>
2023-09-28 00:12:48 +0530

    vnet interface

---
+++ vnetinterface.tf
@@ -0,0 +1,50 @@
+terraform{
+    required_providers{
+        azurerm ={
+            source ="hashicorp/azurerm"
+            version = "3.75.0"
+        }
+    }
+}
+provider "azurerm"{
+    subscription_id =""
+    tenant_id =""
+    client_id =""
+    slient_secret =""
+    features{}
+}
+locals{
+    resource_group_name ="demo_rg"
+    location ="North Europe"
+
+}
+resource "azurerm_resource_group" "demorg"{
+    name ="demo_rg"
+    location =local.location
+}
+resource "azurerm_virtual_network" "vnetwork"{
+    name =""
+    location = local.location
+    resource_group_name = local.resource_group_name
+    address_space =""
+    dns_servers =""
+    depends_on = [azurerm_resource_group.demorg]
+}
+resource "azurerm_subnet" "subnetting1" {
+    name ="subnetA"
+    resource_group_name = local.resource_group_name
+    virtual_network_name = ""
+    address_prefix = "10.0.1.0/24"
+  
+}
+resource "azurerm_network_interface" "demonetwork"{
+    name =""
+    location =local.location
+    resource_group_name =local.resource_group_name
+
+    ip_configuration{
+        name ="internal"
+        subnet_id =azurerm_subnet.subnetting1.id  #Subnet ID automatically inetracts with Azure and locates the Subnet ID which comes in use.        private_ip_address_allocation ="Dynamic"
+
+    }
+}
