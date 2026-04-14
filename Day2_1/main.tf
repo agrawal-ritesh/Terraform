7e38ec6e000975be96ccd8b686e3994c8462572d
Ritesh Agrawal <riteshagarwal1497@gmail.com>
2026-04-14 22:18:43 +0530

    Conflict resolved

--- Day2_1/main.tf
+++ Day2_1/main.tf
@@ -7,26 +7,26 @@ resource "azurerm_resource_group" "rg" {
 
 module "network" {
     source = "./modules/network"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
     location = var.location
 }
 
 module "subnet" {
     source = "./modules/subnet"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
     location = var.location
     vnet_name = module.network.vnet_name
 }
 
 module "nsg" {
     source = "./modules/nsg"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
     location = var.location
 }
 
 module "nic" {
     source = "./modules/nic"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
     location = var.location
     subnet_id = module.subnet.subnet_id
     nsg_id = module.nsg.nsg_id
@@ -34,7 +34,7 @@ module "nic" {
 
 module "vm" {
     source = "./modules/vm"
-    resource_group_name = var.resource_group_name
+    resource_group_name = azurerm_resource_group.rg.name
     location = var.location
     nic_id = module.nic.nic_id
     admin_username = var.admin_username
