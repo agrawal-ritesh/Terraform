f121fa2f669727e997941f65b7b3b34b050162fd
Ritesh Agrawal <riteshagarwal1497@gmail.com>
2026-04-16 20:06:33 +0530

    Conflicts resolved

--- Day3/HA_VMs/main.tf
+++ Day3/HA_VMs/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azure_virtual_network" "vnet" {
     name = "vnet-us"
     location = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.vnet_name
+    resource_group_name = azurerm_resource_group.rg.vnet.name
     address_space = ["10.0.0.0/16"]
     tags = var.tags
 }
@@ -20,12 +20,12 @@ resource "azurerm_subnet" "subnet" {
 }
 
 module "vm" {
-    source = ./modules/vm
+    source = "./modules/vm"
     vm_count = var.vm_count
     vm_size = var.vm_size
     location = var.location
     resource_group_name = azurerm_resource_group.rg.name
-    subnet_id = azurerm_subnet.subnet_id
+    subnet_id = azurerm_subnet.subnet.id
     admin_username = var.admin_username
     admin_password = var.admin_password
     tags = var.tags
