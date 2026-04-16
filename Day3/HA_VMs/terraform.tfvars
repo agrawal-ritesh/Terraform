340d7f0e20711ffdd68ccefba4c643220af38468
Ritesh Agrawal <riteshagarwal1497@gmail.com>
2026-04-16 18:49:42 +0530

    Day 3

---
+++ Day3/HA_VMs/terraform.tfvars
@@ -0,0 +1,10 @@
+
+admin_username = "azureuser"
+
+admin_password = "Passw0rd"
+
+tags = {
+    Environment = "Production"
+    Project = " High Availability"
+    Owner = "MercX"
+}
---
+++ Day3/HA_VMs/variables.tf
@@ -0,0 +1,20 @@
+
+variable "location" {
+    default = "East US"
+}
+
+variable "resource_group_name" {
+    default = "rg-HA_VMs"
+}
+
+variable "admin_username" {}
+
+variable "admin_password" {}
+
+variable "vm_size" {
+    default = "Standard_D2as_v5"
+}
+
+variable "tags" {
+    type = map(stirng)  # This means the variable must be a map (key - value pair) where every value is s string.
+}
