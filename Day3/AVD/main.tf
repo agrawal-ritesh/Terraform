9967347eaebf127feb6340dad7660b4cb711f474
Ritesh Agrawal <riteshagarwal1497@gmail.com>
2026-04-17 15:30:38 +0530

    AVD resources

---
+++ Day3/AVD/main.tf
@@ -0,0 +1,17 @@
+
+
+resource "azurerm_resource_group" "rg" {
+    name = var.resource_group_name
+    location = var.location
+    tags = var.tags
+}
+
+module "avd" {
+    source = "./modules/avd"
+    locaiton = var.location
+    resource_group_name = azurerm_resource_group.rg.name
+    vm_count = var.vm_count
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+    tags = var.tags
+}
---
+++ Day3/AVD/terraform.tfvars
@@ -0,0 +1,12 @@
+
+
+admin_username = "adminuser"
+
+admin_password = "Passw0rd"
+
+tags ={
+    Environment = "Production"
+    Project = "AVD"
+}
+
+
